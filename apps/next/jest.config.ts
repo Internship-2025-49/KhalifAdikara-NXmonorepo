6ad1fc1939628a053392a7720e8e3734e5c20fd1
Khahaha10 <knurfattah@gmail.com>
2025-02-24 14:24:27 +0700

    refactor: restructure Next.js application configuration #3

--- apps/next/jest.config.ts
+++ apps/next/jest.config.ts
@@ -1,10 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
 export default {
-  displayName: 'nx-application',
+  displayName: 'next',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/nx-application',
+  coverageDirectory: '../../coverage/apps/next',
 };

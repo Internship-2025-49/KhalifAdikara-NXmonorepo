92e7b000cdef869995eefa86dbe0e92872f601a4
Khahaha10 <knurfattah@gmail.com>
2025-02-25 15:08:54 +0700

    refactor: move component ui to a new folder #4

--- apps/shared/components/providers/tanstack-provider.tsx
+++ apps/shared/components/providers/tanstack-provider.tsx
@@ -4,13 +4,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 
 interface TanstackProviderProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export const TanstackProvider = ({ children }: TanstackProviderProps) => {
-  const [queryClient] = useState(() => new QueryClient());
+    const [queryClient] = useState(() => new QueryClient());
 
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    );
 };

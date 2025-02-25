79c4196a8b1ebe72a73f65ceab90bcaa5fbd8512
Khahaha10 <knurfattah@gmail.com>
2025-02-25 08:51:37 +0700

    feat: update user schema to make id optional #3

--- apps/shared/types/userSchema.ts
+++ apps/shared/types/userSchema.ts
@@ -1,11 +1,11 @@
 import { z } from "zod"
 
 export const userSchema = z.object({
-    id: z.number(),
+    id: z.number().optional(),
     username: z.string().min(3),
     name: z.string().min(3),
     address: z.string().min(3),
-    phone: z.string().min(13).regex(/^(?:\+62|62|0)[2-9]\d{7,11}$/, { message: "Number must start with +62" }),
+    phone: z.string().min(10).regex(/^(?:\+62|62|0)[2-9]\d{7,11}$/, { message: "Number must start with '08'" }),
 })
 
 export type userForm = z.infer<typeof userSchema>;

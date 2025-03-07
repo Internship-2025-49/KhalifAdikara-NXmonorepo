b0cf96f29e58074874865c9273bcf47f17b68bfb
Khahaha10 <knurfattah@gmail.com>
2025-03-07 15:58:40 +0700

    refactor: move user CRUD operations file #3

--- apps/next/src/app/utils/queries/users/query.ts
+++ apps/next/src/app/utils/queries/users/query.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @nx/enforce-module-boundaries */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { UserAddModel } from '../../../../../../shared/types/users';
-import { getApiKey, getAuthToken } from '../../../utils/authHelpers';
+import { getApiKey, getAuthToken } from '../../authHelpers';
 
-export async function getUsers() {
+export const getUsers = async () => {
     try {
         const token = await getAuthToken();
         const apiKey = await getApiKey(token);
@@ -19,9 +19,9 @@ export async function getUsers() {
     } catch (error: any) {
         throw new Error(error.message);
     }
-}
+};
 
-export async function createUser(userData: UserAddModel) {
+export const createUser = async (userData: UserAddModel) => {
     try {
         const token = await getAuthToken();
         const apiKey = await getApiKey(token);
@@ -40,4 +40,64 @@ export async function createUser(userData: UserAddModel) {
     } catch (error: any) {
         throw new Error(error.message);
     }
-}
+};
+
+export const getUserById = async (params: { id: number }) => {
+    try {
+        const token = await getAuthToken();
+        const apiKey = await getApiKey(token);
+
+        const res = await fetch(`http://localhost:3000/api/users/data/${params.id}`, {
+            next: { revalidate: 10 },
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'api-key': apiKey
+            }
+        });
+
+        return await res.json();
+    } catch (error: any) {
+        throw new Error(error.message);
+    }
+};
+
+export const updateUser = async (params: { id: number }, userData: UserAddModel) => {
+    try {
+        const token = await getAuthToken();
+        const apiKey = await getApiKey(token);
+
+        const res = await fetch(`http://localhost:3000/api/users/data/${params.id}`, {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'api-key': apiKey,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData),
+        });
+
+        return await res.json();
+    } catch (error: any) {
+        throw new Error(error.message);
+    }
+};
+
+export const deleteUser = async (params: { id: number }) => {
+    try {
+        const token = await getAuthToken();
+        const apiKey = await getApiKey(token);
+
+        const res = await fetch(`http://localhost:3000/api/users/data/${params.id}`, {
+            next: { revalidate: 10 },
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'api-key': apiKey
+            }
+        });
+
+        return await res.json();
+    } catch (error: any) {
+        throw new Error(error.message);
+    }
+};
